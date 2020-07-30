ccbeaccc5adf7d98658e5a40a4951ab3388c08c9
Sidnei Farias <sidnei.farias@sirius.com.br>
2020-07-30 10:04:55 -0300

    Updates

---
+++ precache-manifest.583a3c7456bca3420938b1bbba25fb3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e481a4302e99f21b29a3ae784f8b20a0",
+    "url": "/Contact-Github-API/index.html"
+  },
+  {
+    "revision": "134e09bde2e0e822b379",
+    "url": "/Contact-Github-API/static/css/main.325f2e66.chunk.css"
+  },
+  {
+    "revision": "b8f1239418ad4477d2a5",
+    "url": "/Contact-Github-API/static/js/2.ce16f9ef.chunk.js"
+  },
+  {
+    "revision": "134e09bde2e0e822b379",
+    "url": "/Contact-Github-API/static/js/main.e3f11c90.chunk.js"
+  },
+  {
+    "revision": "24c60db22a37b9bff0e5",
+    "url": "/Contact-Github-API/static/js/runtime-main.c0960eec.js"
+  }
+]);
